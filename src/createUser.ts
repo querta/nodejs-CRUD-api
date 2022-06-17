1fcb79e8100dce160b7ba2a1f2e2e6af74e052ce
qurta <afy@dr.com>
2022-06-17 17:20:17 +0200

    feat: implement function getUser for post requests

---
+++ src/createUser.ts
@@ -0,0 +1,31 @@
+import http from 'http';
+import * as err from './errors';
+import { users } from './index';
+import { ICandidate } from './interfaces';
+
+const getRequestData = (req: http.IncomingMessage): Promise<ICandidate> =>
+    new Promise((resolve, reject) => {
+        try {
+            let body = '';
+            req.on('data', (chunk) => {
+                body += chunk.toString();
+            });
+            req.on('end', () => {
+                resolve(JSON.parse(body));
+            });
+        } catch (error) {
+            reject(error);
+        }
+});
+
+const createUser = async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    if (req.url === '/api/users') {
+            const data: ICandidate = await getRequestData(req);
+            if (!data.name || !data.age) { err.body(); }
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            const newUser = await users.createUser(data);
+            res.end(JSON.stringify(newUser));
+    } else { err.route(); }
+};
+
+export default createUser;

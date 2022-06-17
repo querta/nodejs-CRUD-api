050f0d694ccecfc51aa593b506a324a486319673
qurta <afy@dr.com>
2022-06-17 17:20:48 +0200

    refactor: index.ts delete unused pieces of code

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import http from 'http';
 import 'dotenv/config';
 // import { ICandidate, IUser } from './interfaces';
 import userDB from './UsersDBclass';
-import processGet from './processGet';
-import processPost from './processPost';
+import getUser from './getUser';
+import createUser from './createUser';
 import * as err from './errors';
 // import User from './types';
 
@@ -17,12 +17,10 @@ users.createUser({ name: 'Vagan', age: 33, hobbies: ['read, swim'] });
 const reqProcess = async (req: http.IncomingMessage, res: http.ServerResponse) => {
     switch (req.method!) {
         case 'GET':
-            await processGet(req, res);
-            // res.end();
+            await getUser(req, res);
             break;
         case 'POST':
-            await processPost(req, res);
-            // res.end();
+            await createUser(req, res);
             break;
         default:
             err.method();
@@ -36,34 +34,6 @@ const server = http.createServer(async (req, res) => {
     } catch (error: any) {
         err.Handler(error, res);
     }
-    // if (req.url!.match(/\/api\/users\/\w+/)) {
-    //     console.log('1');
-    // } else {
-    //     console.log('2');
-    // }
-    // let urls = req.url?.split('/');
-    // // console.log(urls);
-    // // res.write(JSON.stringify(req.url));
-    // // console.log(req.url);
-    // if (urls && urls[1] === 'api' && urls[2] === 'users') {
-    //     console.log('111');
-    //     if (req.method === 'GET') {
-    //         if (urls[3]) {
-    //             res.writeHead(200, { 'Content-Type': 'application/json' });
-    //             if (idValidate(urls[3])) throw new Error();
-    //             res.write(JSON.stringify(users.getUserById));
-    //             res.end();
-    //         } else {
-    //             res.writeHead(200, { 'Content-Type': 'application/json' });
-    //             res.write(JSON.stringify(users));
-    //             res.end();
-    //         }
-    //     }
-    // } else {
-    //     res.writeHead(404, { 'Content-Type': 'text/html' });
-    //     res.write('Route not found');
-    //     res.end();
-    // }
 });
 const PORT = process.env.SERVER_PORT || 4000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
